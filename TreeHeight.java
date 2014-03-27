c3deea50ca31148166eb3dc227cec58d54544005
alexanderst <alexander.stoyakin@gmail.com>
2014-03-27 18:10:33 +0200

    Create TreeHeight.java

---
+++ TreeHeight.java
@@ -0,0 +1,16 @@
+public class BinaryTree {
+    class Tree {
+        public int x;
+        public Tree l;
+        public Tree r;
+    }
+    
+    public int solution(Tree T) {        
+        int left_size = 0, right_size = 0;
+        if (null != T.l)
+            left_size = 1 + solution(T.l);
+        if (null != T.r)
+            right_size = 1 + solution(T.r);           
+        return Math.max(left_size, right_size);
+    }
+}
