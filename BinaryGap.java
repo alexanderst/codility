7f73e0babbf4c98684608212a562fb091aabe0fd
alexanderst <alexander.stoyakin@gmail.com>
2014-03-27 15:50:18 +0200

    Create BinaryGap.java
    Solution for codility training task Binary Gap

---
+++ BinaryGap.java
@@ -0,0 +1,24 @@
+public class BinaryGap {
+    
+
+    public static int Solution(int N) {
+
+        int max = 0, cnt = 0, trailing = 1;
+        String binaryRepr = Integer.toBinaryString(N);                
+        for (int i = binaryRepr.length() - 1; i >= 0 ; --i) {              
+            if (binaryRepr.charAt(i) == '0' && trailing == 0) {                
+                ++cnt;
+                if (cnt > max)
+                    max = cnt;
+            }
+            else {
+                trailing  = 0;
+                cnt = 0;
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Solution(5));
+    }
