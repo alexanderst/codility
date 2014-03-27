e35fd7ca5a73801145944402f7736fa249421a40
alexanderst <alexander.stoyakin@gmail.com>
2014-03-27 16:00:12 +0200

    Update BinaryGap.java
    100% this time :)

--- BinaryGap.java
+++ BinaryGap.java
@@ -1,13 +1,15 @@
 public class BinaryGap {
     
     public static int Solution(int N) {
-        int max = 0, cnt = 0, trailing = 1;
-        String binaryRepr = Integer.toBinaryString(N);                
+       int max = 0, cnt = 0, trailing = 1;
+        String binaryRepr = Integer.toBinaryString(N); 
         for (int i = binaryRepr.length() - 1; i >= 0 ; --i) {              
-            if (binaryRepr.charAt(i) == '0' && trailing == 0) {                
-                ++cnt;
-                if (cnt > max)
-                    max = cnt;
+            if (binaryRepr.charAt(i) == '0') {                
+                if (trailing == 0) {
+                    ++cnt;
+                    if (cnt > max)
+                        max = cnt;
+                }
             }
             else {
                 trailing  = 0;
