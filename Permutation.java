38fb5df0f7bfcb88e5f2ec5efc9c34b8ad4a84df
alexanderst <alexander.stoyakin@gmail.com>
2014-05-30 19:56:48 +0300

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -5,15 +5,15 @@ import java.util.LinkedHashSet;
 
 public class Permutation {
 	public static int solution(int[] A) {
-		int sum1 = 0, sum2 = 0;
+		int sum_indexes = 0, sum_values = 0;
 		java.util.Set<Integer> tmp = new LinkedHashSet<Integer>();
 		for (int i = 0; i < A.length; ++i) {
 			if (A[i] > A.length || A[i] < 1 || !tmp.add(A[i])) {
 				return 0;
 			}
-			sum1 += i+1;
-			sum2 += A[i];
+			sum_indexes += i+1;
+			sum_values += A[i];
 		}
-		return (sum1 == sum2 ? 1 : 0);
+		return (sum_indexes == sum_values ? 1 : 0);
 	}
 }
