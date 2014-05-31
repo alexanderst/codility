71daf94c90c17095fd93230488ce5aa164f7a0f8
alexanderst <alexander.stoyakin@gmail.com>
2014-05-31 18:55:11 +0300

    Create TapeEquilibrium.java

---
+++ TimeComplexity/TapeEquilibrium.java
@@ -0,0 +1,21 @@
+package TimeComplexity;
+
+public class TapeEquilibrium {
+	public static int solution(int[] A) {
+			int val = -1;
+			int sum_left = 0, sum_right = 0;
+			
+			for (int i = 1; i < A.length; ++i) {
+				sum_right += A[i];
+			}
+			for (int i = 1; i < A.length; ++i) {
+			    sum_left += A[i-1];
+				int attempt = Math.abs(sum_left - sum_right);
+				sum_right -= A[i];
+				if (-1 == val || attempt < val) {
+				    val = attempt;
+				}
+			}
+			return val;
+	    }
+}
